7b1f67fbd190c2ec2ea2d6a8356904dadb4a3d5c
g.salvio <giuseppe.salvio@finconsgroup.com>
2022-02-03 16:32:54 +0100

    fino allo spare

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -2,23 +2,60 @@
 public class Game {
 
     private int score;
+    private boolean spare;
+    private int actualFrameScore;
     private boolean firstRoll;
+    private int frame;
+    private boolean previousRollWasAstrike;
 
     public Game() {
         this.score = 0;
+        this.actualFrameScore = 0;
+        this.spare = false;
         this.firstRoll = true;
+        this.frame = 0;
     }
 
     void roll(int pinnedKnoked) {
-        score = score + pinnedKnoked;
-        if (!firstRoll && score == 10) {
-            score = score + pinnedKnoked;
-        }
+        sePrimoTiro();
+        actualFrameScore += pinnedKnoked;
+        score += pinnedKnoked;
+        score += getBonus(pinnedKnoked);
+        spare = isSpare();
         firstRoll = !firstRoll;
+    }
+
+    private boolean isSpare() {
+        return actualFrameScore == 10 && !firstRoll;
+    }
+
+    private boolean isStrike() {
+        return actualFrameScore == 10 && firstRoll;
+    }
+
+    private int getBonus(int pinnedKnoked) {
+        if (spare) {
+            spare = false;
+            return pinnedKnoked;
+        }
+        if(isStrike()){
+            previousRollWasAstrike = true;
+            return 10;
+        }
+        return 0;
+    }
+
+    private void sePrimoTiro() {
+        if (firstRoll) {
+            actualFrameScore = 0;
+        }
     }
 
     int score() {
         return score;
     }
 
+    public int getActualFrameScore() {
+        return actualFrameScore;
+    }
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,5 +1,3 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -30,24 +28,39 @@ class GameTest {
     }
 
     @Test
-    void whenSecondoRollSpareShoulReturnScoreAsSummOfPreviouseScoreAndDoubleOfSecondRoll() {
+    void whenSpareFirstFrame() {
         game = new Game();
         game.roll(8);
         game.roll(2);
-        assertEquals(8+2*2 ,game.score());
+        game.roll(3);
+        assertEquals(8+2+3+3 ,game.score());
     }
 
-//    @Test
-//    void booooo() {
-//        game = new Game();
-//        game.roll(3);
-//        game.roll(4);
-//        game.roll(8);
-//        game.roll(2);
-//        assertEquals(3+4+8+2*2 ,game.score());
-//    }
+
+    @Test
+    void whenSpareInSecondFrame() {
+        game = new Game();
+        game.roll(1);
+        game.roll(1);
+        assertEquals(1+1 ,game.getActualFrameScore());
+        game.roll(8);
+        game.roll(2);
+        assertEquals(8+2 ,game.getActualFrameScore());
+        game.roll(3);
+        game.roll(4);
+        assertEquals(3+4 ,game.getActualFrameScore());
+        assertEquals(1+1+8+2+3+3+4 ,game.score());
+    }
 
 
+    @Test
+    void whenStrikeFirstFrame() {
+        game = new Game();
+        game.roll(10);
+        game.roll(3);
+        game.roll(4);
+        assertEquals(10+ 3+4 + 3+4 ,game.score());
+    }
 
 
 }
